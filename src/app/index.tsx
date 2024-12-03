2af3a95bf24d795628b51109a7c3e326b3679f9f
Marcos Camargo <121471879+marcoscamarg0@users.noreply.github.com>
2024-12-03 14:47:03 -0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Dimensions, StatusBar } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, StatusBar, Image } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { FontAwesome } from '@expo/vector-icons';
 import tw from 'twrnc';
@@ -29,9 +29,9 @@ const RaceDashboard = () => {
             <Text style={tw`text-white text-3xl font-bold`}>3:15:01</Text>
           </View>
           <View style={tw`bg-gray-800 p-4 rounded-lg w-[48%]`}>
-            <Text style={tw`text-orange-500 text-lg`}>VOLTA 1</Text>
+            <Text style={tw`text-orange-500 text-lg`}>VOLTA ATUAL</Text>
             <Text style={tw`text-white text-3xl font-bold`}>2:10:01</Text>
-            <View style={tw`bg-white h-1 mt-2 rounded-full`}></View>
+            
           </View>
         </View>
 
@@ -39,12 +39,12 @@ const RaceDashboard = () => {
           <Text style={tw`text-orange-500 text-lg mb-2`}>RANKING</Text>
           <View style={tw`space-y-4`}>
             {['CORREDOR 1', 'CORREDOR 2', 'CORREDOR 3', 'CORREDOR 4', 'CORREDOR 5', 'CORREDOR 6'].map((runner, index) => (
-              <View key={index} style={tw`flex-row justify-between items-center bg-gray-800 p-3 rounded-lg`}>
+              <View key={index} style={tw`flex-row justify-between items-center bg-gray-800 p-2 rounded-lg`}>
                 <View style={tw`flex-row items-center`}>
                   <Text style={tw`text-white text-lg font-bold`}>{index + 1}º</Text>
-                  <Text style={tw`ml-3 text-white text-lg`}>{runner}</Text>
+                  <Text style={tw`ml-4 text-white text-lg`}>{runner}</Text>
                 </View>
-                <FontAwesome name="user" size={20} color="white" />
+                <FontAwesome name="user" size={16} color="white" />
               </View>
             ))}
           </View>
@@ -64,9 +64,11 @@ const RaceDashboard = () => {
         </View>
 
         <View style={tw`items-center`}>
-          <View style={tw`w-24 h-24 bg-gray-800 rounded-full items-center justify-center`}>
-            <Text style={tw`text-white text-lg font-bold`}>VTR</Text>
-          </View>
+          <Image 
+            source={require('../assets/logo.png')} 
+            style={tw`w-30 h-30 `} 
+            resizeMode="contain"
+          />
         </View>
       </View>
     </View>
